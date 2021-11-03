ea757c8a267f4307e0fb4fe6c25553c798b7d6f6
YamamuraYuya <sales@best-rise.monster>
2021-11-04 05:50:03 +0900

    `Stagger○○Animations`のコンストラクターが長くなっていて読み辛かったので、引数の改行を整理した

--- lib/stagger_grid_page/children/stagger_grid_animations.dart
+++ lib/stagger_grid_page/children/stagger_grid_animations.dart
@@ -2,43 +2,22 @@ import 'package:flutter/material.dart';
 
 class StaggerGridAnimations extends StatelessWidget {
   StaggerGridAnimations(this.controller)
-      : opacity = Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
+      : opacity = Tween<double>(begin: 0.0, end: 1.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.0,
-              0.100,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.0, 0.100, curve: Curves.ease),
           ),
         ),
-        width = Tween<double>(
-          begin: 25.0,
-          end: 50.0,
-        ).animate(
+        width = Tween<double>(begin: 25.0, end: 50.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.125,
-              0.250,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.125, 0.250, curve: Curves.ease),
           ),
         ),
-        height = Tween<double>(
-          begin: 25.0,
-          end: 50.0,
-        ).animate(
+        height = Tween<double>(begin: 25.0, end: 50.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.250, 0.375, curve: Curves.ease),
           ),
         ),
         padding = EdgeInsetsTween(
@@ -47,11 +26,7 @@ class StaggerGridAnimations extends StatelessWidget {
         ).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.250, 0.375, curve: Curves.ease),
           ),
         ),
         borderRadius = BorderRadiusTween(
@@ -60,24 +35,13 @@ class StaggerGridAnimations extends StatelessWidget {
         ).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.375,
-              0.500,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.375, 0.500, curve: Curves.ease),
           ),
         ),
-        color = ColorTween(
-          begin: Colors.blue,
-          end: Colors.orange,
-        ).animate(
+        color = ColorTween(begin: Colors.blue, end: Colors.orange).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.500,
-              0.750,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.500, 0.750, curve: Curves.ease),
           ),
         );
 
--- lib/stagger_single_page/children/stagger_single_animations.dart
+++ lib/stagger_single_page/children/stagger_single_animations.dart
@@ -2,43 +2,22 @@ import 'package:flutter/material.dart';
 
 class StaggerSingleAnimation extends StatelessWidget {
   StaggerSingleAnimation(this.controller)
-      : opacity = Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
+      : opacity = Tween<double>(begin: 0.0, end: 1.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.0,
-              0.100,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.0, 0.100, curve: Curves.ease),
           ),
         ),
-        width = Tween<double>(
-          begin: 50.0,
-          end: 150.0,
-        ).animate(
+        width = Tween<double>(begin: 50.0, end: 150.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.125,
-              0.250,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.125, 0.250, curve: Curves.ease),
           ),
         ),
-        height = Tween<double>(
-          begin: 50.0,
-          end: 150.0,
-        ).animate(
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.250, 0.375, curve: Curves.ease),
           ),
         ),
         padding = EdgeInsetsTween(
@@ -47,11 +26,7 @@ class StaggerSingleAnimation extends StatelessWidget {
         ).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.250, 0.375, curve: Curves.ease),
           ),
         ),
         borderRadius = BorderRadiusTween(
@@ -60,24 +35,13 @@ class StaggerSingleAnimation extends StatelessWidget {
         ).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.375,
-              0.500,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.375, 0.500, curve: Curves.ease),
           ),
         ),
-        color = ColorTween(
-          begin: Colors.blue,
-          end: Colors.orange,
-        ).animate(
+        color = ColorTween(begin: Colors.blue, end: Colors.orange).animate(
           CurvedAnimation(
             parent: controller,
-            curve: Interval(
-              0.500,
-              0.750,
-              curve: Curves.ease,
-            ),
+            curve: Interval(0.500, 0.750, curve: Curves.ease),
           ),
         );
 
